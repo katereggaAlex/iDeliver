7e3c37e03e1fdb075f3ea824658c4e019c7a98c6
katereggaAlex <77050709+katereggaAlex@users.noreply.github.com>
2021-07-19 12:36:19 +0300

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyABDIXH96KAddSmxiZQTj0t91p-bgVrT3w",
+  authDomain: "ideliver-app-72fdb.firebaseapp.com",
+  databaseURL: "https://ideliver-app-72fdb-default-rtdb.firebaseio.com",
+  projectId: "ideliver-app-72fdb",
+  storageBucket: "ideliver-app-72fdb.appspot.com",
+  messagingSenderId: "179339039786",
+  appId: "1:179339039786:web:1d0d282dba4a6fc8425f53",
+  measurementId: "G-X11KG6NC0Q"
 };
 
 /*
