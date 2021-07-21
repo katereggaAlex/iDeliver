a2017441a66ad411b5233b7ba79e48460018bd8a
katereggaAlex <77050709+katereggaAlex@users.noreply.github.com>
2021-07-21 13:36:00 +0300

    new changes

--- src/app/pages/services/data.service.spec.ts
+++ src/app/pages/services/data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiServiceService } from './api-service.service';
+import { DataService } from './data.service';
 
-describe('ApiServiceService', () => {
-  let service: ApiServiceService;
+describe('DataService', () => {
+  let service: DataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiServiceService);
+    service = TestBed.inject(DataService);
   });
 
   it('should be created', () => {
---
+++ src/app/pages/services/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+
+//setting active user
+
+  getActiveUser() {
+    return JSON.parse(localStorage.getItem('activeUser'));
+  }
+//function to set vendor shop
+  setMyShop(data){
+    return localStorage.setItem('myshop', JSON.stringify(data));
+
+  }
+//function to get vendor shop
+  getMyShop() {
+    return JSON.parse(localStorage.getItem('myshop'));
+  }
+//function to get Active shop
+  getActiveShop() {
+    return JSON.parse(localStorage.getItem('activeShop'));
+  }
+
+
+}
